addcd5d9115af8e9db32c5d6b07f0a45126f7d03
Cindyring <Cindyring_Dream@outlook.com>
2023-03-19 21:14:36 +0800

    insert and delete function of sqlist

--- 20230318sqlist/Sqlist/Sqlist/SqList.c
+++ 20230318sqlist/Sqlist/Sqlist/SqList.c
@@ -17,6 +17,7 @@ Status DestroyList(SqList* L)
 	free(L->elem);
 	L->length = 0;
 	L->listsize = 0;
+	return OK;
 }
 
 Status ClearList(SqList* L)
@@ -40,11 +41,40 @@ int ListLength(SqList L)
 
 Status GetElem(SqList L, int i, ElemType* e)
 {	
-	if (L.length > i && i>0)
+	if (L.length >= i && i>0)
 	{
-		*e = L.elem[i];
+		*e = L.elem[i-1];
 		if (e) return OK;
 	}
 	else return NO;
-	
+}
+
+Status ListInsert(SqList* L, int i, ElemType e)
+{
+	if (i<1 || i>L->length + 1) return NO;
+	if (L->length >= L->listsize)
+	{
+		ElemType *newbase = 
+			(ElemType*)realloc(L->elem, (L->listsize + LISTINCREAMENT) * sizeof(ElemType));
+		if (!newbase) exit(NO);
+		L->elem = newbase;
+		L->listsize += LISTINCREAMENT;
+	}
+	ElemType *q = &(L->elem[i - 1]);
+	for (ElemType *p = &(L->elem[(L->length - 1)]); p >= q; --p) *(p + 1) = *p;
+	*q = e;
+	++(L->length);
+
+	return OK;
+}
+
+Status ListDelete(SqList* L, int i)
+{
+	if (i<1 || i>L->length) return NO;
+	ElemType *p = &(L->elem[i - 1]);
+	ElemType* q = L->elem + L->length - 1;
+	for (++p; p <= q; ++p) *(p - 1) = *p;
+	--(L->length);
+	return OK;
+
 }
--- 20230318sqlist/Sqlist/Sqlist/main.c
+++ 20230318sqlist/Sqlist/Sqlist/main.c
@@ -6,6 +6,11 @@ int main()
 	SqList test;
 	InitList(&test);
 	printf("%d %d", test.length, test.listsize);
-	DestroyList(&test);
-	printf("\n%d %d", test.length, test.listsize);
+	
+	ListInsert(&test, 1, 2);
+	int a;
+	GetElem(test, 1, &a);
+	printf("\n%d %d %d", test.length, test.listsize,a);
+
+
 }
