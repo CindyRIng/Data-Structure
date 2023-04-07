77609cd1ff44c297989364cacd4f3dee6b583f04
Cindyring <Cindyring_Dream@outlook.com>
2023-04-07 13:20:36 +0800

    optimize the test file

--- 20230328StaticSqList/StaticSqList/test.c
+++ 20230328StaticSqList/StaticSqList/test.c
@@ -3,12 +3,35 @@
 
 void main()
 {
+	//初始化线性表
 	SqList L;
-	initList(&L);
-	for (int i = 1; i <= 5; i++)
+	printf("正在初始化线性表...");
+	if (initList(&L))
+		printf("线性表初始化成功!");
+	else
+		printf("线性表初始化失败!");
+
+	//向线性表中插入十个元素
+	printf("正在向线性表插入元素...");
+	for (int i = 1; i <= 10; i++)
 	{
-		insert(&L, 1, i);
+		if (insert(&L, 1, i))
+			printf("插入元素%d\n", i);
+		else
+			printf("插入失败");
 	}
-	printf("%d\n", L.length);
+
+	//遍历线性表
+	printf("当前线性表的元素为:\n");
+	ListTraverse(L);
+
+	//删除元素
+	printf("\n正在删除元素...\n");
+	if (Delete(&L, 2))
+		printf("已经删除了第二位元素\n");
+	else
+		printf("删除失败\n");
+
+	printf("当前线性表的元素为:\n");
 	ListTraverse(L);
 }
